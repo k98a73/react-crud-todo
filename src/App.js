7ca3590cadd38907a6dc2508ef8009edb2d155ea
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-06-01 14:45:49 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDom.render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
